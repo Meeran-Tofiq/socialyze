100e81e21c4eecdab7eb2b1de569b1a97eff968c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2025-05-27 23:07:54 +0100

    test(api): add tests for user routes

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,10 +1,16 @@
 // apps/api/vitest.config.ts
 import { defineConfig } from "vitest/config";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config({ path: ".env.test" });
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@api": path.resolve(__dirname, "src"),
+		},
+	},
 	test: {
 		globals: true,
 		environment: "node",
